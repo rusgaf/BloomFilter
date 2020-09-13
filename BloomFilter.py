9062fec84b26526585ecdad41f2e03adb7656bf9
Rus <rustam66637@gmail.com>
2020-09-13 22:56:10 +0300

    Update BloomFilter.py

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,1 +1,19 @@
 class BloomFilter:
+    def __init__(self, f_len):
+        self.filter_len = f_len
+        # создаём битовый массив длиной f_len ...
+
+    def hash1(self, str1):
+        for c int str1:
+            code = ord(c)
+        #реализация
+
+    def hash2(self, str1):
+        #223
+        #...
+
+    def add(self, str1):
+        # добавляем строку str1 в фильтр
+
+    def is_value(self, str1):
+        # проверка, имеется ли строка str1 в фильтре
